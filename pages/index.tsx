45429a5e66e06f1205e68c6e93f8b5c9298a5227
Jamie Joahill <jamie.joahill@gmail.com>
2024-11-23 19:32:36 +0000

    adding a Link to contact.jsx in index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,6 +40,7 @@ export default function Home() {
           <li>This is the fourth line.</li>
         </ol>
         <Link href='/about'>Go to the about page</Link>
+        <Link href='/contact'>Go to the contact page</Link>
         <div className='flex gap-4 items-center flex-col sm:flex-row'>
           <a
             className='rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5'
