39f3bc413a793ec515dc27738050e72544e9a870
Jamie Joahill <jamie.joahill@gmail.com>
2024-11-23 19:31:04 +0000

    Created contact.tsx added Link on index.tsx to redirect to Contact.tsx

---
+++ pages/contact.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function contact() {
+  return (
+    <div>
+      <h2>This is the contact page</h2>
+      <Link href='/'>Go to the home page</Link>
+    </div>
+  )
+}
