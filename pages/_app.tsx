c26b6c5416a502fb0c33f0b83962bd21fdeca933
Jamie Joahill <jamie.joahill@gmail.com>
2024-11-23 19:06:17 +0000

    Fix dyanmic import for preline to resolve deployment issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,11 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      require('preline/dist/preline')
+      import('preline/dist/preline')
+        .then((module) => {})
+        .catch((error: string) => {
+          console.error('Failed to log preline: ', error)
+        })
     }
   })
   return <Component {...pageProps} />
