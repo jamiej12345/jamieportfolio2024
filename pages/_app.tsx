76f83d82ea3bdfd779d62cd6f1e3be8a8747062c
Jamie Joahill <jamie.joahill@gmail.com>
2024-11-23 19:09:06 +0000

    Fixed unused variable issue for dynamic import of preline

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,9 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     if (typeof window !== 'undefined') {
-      import('preline/dist/preline')
-        .then((module) => {})
-        .catch((error: string) => {
-          console.error('Failed to log preline: ', error)
-        })
+      import('preline/dist/preline').catch((error: string) => {
+        console.error('Failed to log preline: ', error)
+      })
     }
   })
   return <Component {...pageProps} />
